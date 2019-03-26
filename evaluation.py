65387be136fac5aa370c7b5191beef25ea85d4d5
wldud8463 <wldud8463@naver.com>
2019-03-26 14:42:48 +0900

    file arrangement

--- evaluation.py
+++
@@ -1,8 +0,0 @@
-def evaluate(answer, predict):
-    tn, fp, fn, tp = confusion_matrix(answer, predict).ravel()
-    print(tp)
-    print(fp)
-    print(float(tp)/(tp + fp))
-
-
-# print evaluate(answer, predict)
