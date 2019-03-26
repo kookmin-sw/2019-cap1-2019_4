19054882d8f5049d96f1ef92f5f361365b91e097
jiisoo <jsmin0415@gmail.com>
2019-03-27 00:28:07 +0900

    install python related package

---
+++ src/recommendation/aws_ec2/install_required_package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get update
+
+# python install
+sudo apt-get install python
+
+# pip install
+sudo apt-get install python-pip
+
+# install python-mysqldb
+sudo apt-get install python-mysqldb
+
+# it is not certain to install below package. if connection between DB and ec2 is failed, run this command. 
+# sudo pip install mysql-connector-python-rf
+
+
