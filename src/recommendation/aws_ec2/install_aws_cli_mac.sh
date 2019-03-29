63f1249b853e333a9c2ac791b490b34ddc369914
jiisoo <jsmin0415@gmail.com>
2019-03-29 13:41:30 +0900

    move file and add comments

---
+++ src/recommendation/aws_ec2/install_aws_cli_mac.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#for mac
+
+# after install home brew, install python2 with pip
+brew install python@2
+
+#if you want to install python3 
+# brew install python
+
+#install aws-cli
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
+aws --version
