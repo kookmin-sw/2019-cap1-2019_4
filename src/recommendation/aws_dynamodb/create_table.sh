258efef181e1433111eb6b2ce02fdbc904138048
jiisoo <jsmin0415@gmail.com>
2019-04-01 21:06:33 +0900

    remove comments and modify indent

--- src/recommendation/aws_dynamodb/create_table.sh
+++ src/recommendation/aws_dynamodb/create_table.sh
@@ -1,10 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-# create table in Dynamodb
 aws dynamodb create-table \
     --table-name Recommendation \
     --attribute-definitions \
-        AttributeName=user_id,AttributeType=N \ # Partition Key
-        AttributeName=update_ver,AttributeType=N \  #Sort Key
+        AttributeName=user_id,AttributeType=N \
+        AttributeName=update_ver,AttributeType=N \
     --key-schema AttributeName=user_id,KeyType=HASH AttributeName=update_ver,KeyType=RANGE \
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
