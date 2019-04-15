0a2385d16a80ece2aea547f24ea404fdb20e161e
HwangSoojin <blue8959@naver.com>
2019-04-15 10:39:39 +0900

    path 지정 및 해당 폴더에 create 시 업로드 되도록 코드 수정

--- src/face_detection/local_aws_trigger/local_to_aws.py
+++ src/face_detection/local_aws_trigger/local_to_aws.py
@@ -21,19 +21,20 @@ s3 = boto3.client('s3')
 i = inotify.adapters.Inotify()
 
 # monitor specific folders
-i.add_watch('./')
+# 기본 dir 에 python 파일이 존재하고, jetson 에 sd card 에서 detect 한 이미지가 업로드되는 상황
+watch_path = '../../media/nvidia/test_sd'
+i.add_watch(watch_path)
 
 for event in i.event_gen(yield_nones = False):
     (header, type_names, path, saved_filename) = event
     # print("PATH=[{}]\tFILENAME=[{}]\tEVENT_TYPES={}".format(path, filename, type_names))
 
-    # 모니터 하고 있는 폴더에 파일이 들어올 때만 실행
-    if type_names[0] == 'IN_MOVED_TO':
+    # 모니터 하고 있는 폴더에 파일이 생성될 때만 실행
+    if type_names[0] == 'IN_CREATE':
 
         # type_names[0]의 type은 유니코드인데
         # s3에 올리기 위해서는 string이어야 하기 때문에 "utf-8"로 인코딩
         upload_file_name = saved_filename.encode("utf-8")
-
 
         # *S3 업로드 설정*
         # s3에 올리고자 하는 파일의 이름(폴더에 저장된 파일의 이름)
@@ -43,9 +44,10 @@ for event in i.event_gen(yield_nones = False):
         bucket_name = 'flexadstest'
 
         # s3에서의 objectname 설정
-        # (폴더에 저장된 파일의 이름 그대로 s3에 저장하고 싶다면,
-        # objectname = filename)
-        objectname = 'FlexAds_logo_init.png'
+        objectname = filename
+        # objectname = 'FlexAds_logo_init.png'
 
         # upload the file to AWS S3
-        s3.upload_file(filename, bucket_name, objectname)
+        # 업로드 할 파일의 이름을 지정할 때, 본 파일과 같은 경로에 저장할 수도 있기 떄문에
+        # 파일 이름에 경로까지 지정해주어야 업로드가 정상적으로 작동함
+        s3.upload_file(watch_path + filename, bucket_name, objectname)
