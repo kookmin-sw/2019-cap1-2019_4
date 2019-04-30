cf2d3afe7c113463c7ce2f1fc47847686b9531a6
wldud8463 <48198249+wldud8463@users.noreply.github.com>
2019-04-30 13:56:26 +0900

    order_ratio_bychance() 수정

--- src/recommendation/make_feature/order_ratio_bychance.py
+++ src/recommendation/make_feature/order_ratio_bychance.py
@@ -5,14 +5,6 @@ import csv
 import numpy as np
 import re
 
-data_aisles = pd.read_csv("instacart-market-basket-analysis/aisles.csv")
-data_departments = pd.read_csv("instacart-market-basket-analysis/departments.csv")
-data_orders = pd.read_csv("instacart-market-basket-analysis/orders.csv")
-data_product = pd.read_csv("instacart-market-basket-analysis/products.csv")
-data_order_prior = pd.read_csv("instacart-market-basket-analysis/order_products__prior.csv")
-data_order_train = pd.read_csv("instacart-market-basket-analysis/order_products__train.csv")
-
-order_prior =pd.merge(data_order_prior, data_orders, how='inner', on=['order_id'])
 
 """order_ratio_bychance() 유저가 특정 product_id를 처음 샀을때와 마지막 샀을때의 기간동안 방문해서 구매한 비율을 나타냄
 예를 들어 1번 product_id를 order_id( 5,6,8 ) 에 구매를 함 --> 5~8 order_id 기간동안 3번 구매함 --> 결과 3/(8-5+1)=3/4
@@ -24,7 +16,13 @@ order_prior =pd.merge(data_order_prior, data_orders, how='inner', on=['order_id'
 @ chance 계산 마지막에 +1 한 이유는 예문에서 보이듯이 5~8구간은 4이지만 8-5하면 3이 출력돼서..+1
 @order_ratio_bychance : cnt/chance 값으로 user_id가 product_id를 chance 기간동안 구매한 비율
 """
-def order_ratio_bychance(order_prior):
+def order_ratio_bychance():
+    
+    orders_df = pd.read_csv("instacart-market-basket-analysis/orders.csv", usecols=["order_id","user_id","order_number"])
+    prior_df = pd.read_csv("instacart-market-basket-analysis/order_products__prior.csv", usecols = ["order_id", "product_id"])
+    
+    #merge
+    order_prior =pd.merge(prior_df, orders_df, how='inner', on=['order_id'])
     
     cnt =order_prior.groupby(['user_id', 'product_id']).size()
     cnt.name = 'cnt'
@@ -44,5 +42,5 @@ def order_ratio_bychance(order_prior):
     df['order_ratio_bychance'] = df.cnt / df.chance
     
     prior_and_orders2=pd.merge(order_prior, df, how='inner')
-    return prior_and_orders2.drop(['cnt','onb_max','onb_min','chance'], axis=1)
+    return prior_and_orders2.drop(['cnt','onb_max','onb_min','chance','order_id','order_number'], axis=1)
 
