20cb0a7349a53dd84c6f962a21029fc2649274c1
Jigu <jsmin0415@gmail.com>
2019-05-02 21:33:44 +0900

    add font color

--- src/demo_web/get_data.js
+++ src/demo_web/get_data.js
@@ -6,4 +6,8 @@ var user_data = {
 }
 
 var user = document.getElementById('user')
-user.innerHTML = user_data.user_id + "번 고객님, " + user_data.product_name + "은 어떠세요? " + user_data.aisle + "코너에 준비되어있습니다.";
+
+user.innerHTML = "<span style='color: DarkViolet;'>" + user_data.user_id + "</span>"+ "번 고객님, " + 
+				"<span style='color: MediumVioletRed;'>" + user_data.product_name + "</span>" + "은 어떠세요? " + 
+				"<span style='color: SaddleBrown;'>" + user_data.aisle + "</span>" +  "코너에 준비되어있습니다.";
+
