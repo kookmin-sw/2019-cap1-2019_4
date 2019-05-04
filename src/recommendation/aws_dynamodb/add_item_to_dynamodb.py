4ba17bfbf9ef6e93557a50d7395c83e931390c0c
Jigu <jsmin0415@gmail.com>
2019-05-04 13:56:09 +0900

    add recommentdation info to db

--- src/recommendation/aws_dynamodb/add_item_to_dynamodb.py
+++ src/recommendation/aws_dynamodb/add_item_to_dynamodb.py
@@ -2,18 +2,29 @@
 
 from __future__ import print_function # Python 2/3 compatibility
 import boto3
+import pandas as pd
 
-dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+def add_item(table, user_id, product_id):
+    table.put_item(
+        Item={
+        'user_id': user_id,
+        'update_ver' : 1,
+        'product_id': product_id
+        }
+    )
 
-table = dynamodb.Table('Recommendation')
 
-print(table.creation_date_time)
+def main():
+    # connect to dynamodb
+    dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+    # select table to add item
+    table = dynamodb.Table('Recommendation')
+    # the file which contains items
+    recommendation = pd.read_csv("./xgboost_submission.csv")
+   
+    # add all item iteratively
+    for i in range(recommendation.shape[0]):
+        add_item(table, recommendation.loc[i]['user_id'], recommendation.loc[i]['product_id'])
 
-table.put_item(
-   Item={
-        'user_id': 124,
-        'update_ver': 1,
-        'product_id': 24,
-        'product_name' : 'banana'
-    }
-)
+if __name__ == "__main__":
+    main()
