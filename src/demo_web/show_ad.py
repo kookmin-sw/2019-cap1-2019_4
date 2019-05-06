8f430ae8de17bc53ef77b1529bb4587a57f87c31
HwangSoojin <blue8959@naver.com>
2019-05-06 18:21:37 +0900

    고객 및 제품의 정보를 가져와서 HTML에 띄우기

--- src/demo_web/show_ad.py
+++ src/demo_web/show_ad.py
@@ -3,7 +3,7 @@
 # selenium 설치
 # pip install selenium
 
-# 브라우처 driver 설치
+# 브라우처 driver 설치 (현재 크롬 버전 확인 후 설치)
 # - 크롬 드라이브 설치: https://sites.google.com/a/chromium.org/chromedriver/downloads
 
 from selenium import webdriver
@@ -12,9 +12,8 @@ import requests
 
 # Chorme driver가 설치되어 있는 path
 path = './chromedriver'
-
-# 성재오빠 쪽에서 받아오기
-user_id = "124"
+# TODO: 성재오빠 쪽에서 받아오기
+user_id = "23"
 
 # Chrome 창 띄우기
 driver = webdriver.Chrome(path)
@@ -22,10 +21,17 @@ driver = webdriver.Chrome(path)
 URL = 'https://xtvznzjl15.execute-api.us-west-2.amazonaws.com/show?user=' + user_id
 response = requests.get(URL)
 
-user_id = response.json()['user_id']
-user_name = "밍지수"
-product_id = response.json()['product_id']
-product_name = response.json()['product_name']
-bucket_url = response.json()['bucket_url']
+# 고객 아이디 / 이름,  추천 제품 아이디 / 이름 / 진열 구역,  광고 이미지 주소
+uid = response.json()['user_id']
+            # TODO: response.json()['user_name']
+uname = "밍지수"
+            # TODO: 제품 아이디 필요한가?
+pid = response.json()['product_id']
+pname = response.json()['product_name']
+# TODO: paisle = response.json()['product_aisle']
+url = response.json()['bucket_url']
 
-driver.get('file:///C:/Users/blue8/OneDrive/Desktop/sns/myhomepage.html?user=' + str(user_id) + '&user_name=' + user_name + '&product_id=' + str(product_id) + '&product_name=' + product_name + '&bucket_url=' + bucket_url)
+# 고객과 제품의 정보를 포함한 HTML 실행
+# TODO: 새 창이 아닌 현재 창 띄우기 가능한지
+# TODO: ('&product_aisle=' + str(paisle)) 추가, '&product_id=' + str(pid) 불필요시 제거
+driver.get('file:///C:/Users/blue8/OneDrive/Desktop/sns/index.html?user_id=' + str(uid) + '&user_name=' + uname + '&product_id=' + str(pid) + '&product_name=' + pname + '&bucket_url=' + url)
