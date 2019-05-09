f95d36732f68850e187c3ec87b13219e58ee46e5
HwangSoojin <blue8959@naver.com>
2019-05-09 13:34:11 +0900

    광고가 나오는 화면을 전체화면으로 설정하는 옵션 추가

--- src/demo_web/show_ad.py
+++ src/demo_web/show_ad.py
@@ -7,31 +7,45 @@
 # - 크롬 드라이브 설치: https://sites.google.com/a/chromium.org/chromedriver/downloads
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import re
 import requests
+import time
 
 # Chorme driver가 설치되어 있는 path
 path = './chromedriver'
+
+# 광고 화면을 띄우는 창을 전체화면으로 설정
+options = Options()
+# options.add_argument('--start-fullscreen')
+# 창 최대 크기 모드
+# options.add_argument('--start-maximized')
+
 # TODO: 성재오빠 쪽에서 받아오기
-user_id = "23"
+user_id = "77"
+
 
 # Chrome 창 띄우기
-driver = webdriver.Chrome(path)
+driver = webdriver.Chrome(path, chrome_options=options)
 # 사용자의 아이디가 지정되어 있는 url로 접속
 URL = 'https://xtvznzjl15.execute-api.us-west-2.amazonaws.com/show?user=' + user_id
 response = requests.get(URL)
 
 # 고객 아이디 / 이름,  추천 제품 아이디 / 이름 / 진열 구역,  광고 이미지 주소
-uid = response.json()['user_id']
-            # TODO: response.json()['user_name']
+        # TODO: uid = response.json()['user_id']
+uid = 23
+        # TODO: response.json()['user_name']
 uname = "밍지수"
-            # TODO: 제품 아이디 필요한가?
-pid = response.json()['product_id']
 pname = response.json()['product_name']
-# TODO: paisle = response.json()['product_aisle']
+paisle = response.json()['aisle']
 url = response.json()['bucket_url']
 
 # 고객과 제품의 정보를 포함한 HTML 실행
-# TODO: 새 창이 아닌 현재 창 띄우기 가능한지
-# TODO: ('&product_aisle=' + str(paisle)) 추가, '&product_id=' + str(pid) 불필요시 제거
-driver.get('file:///C:/Users/blue8/OneDrive/Desktop/sns/index.html?user_id=' + str(uid) + '&user_name=' + uname + '&product_id=' + str(pid) + '&product_name=' + pname + '&bucket_url=' + url)
+driver.get('file:///C:/Users/blue8/OneDrive/Desktop/sns/index.html?user_id=' + str(uid) + '&user_name=' + uname + '&product_name=' + pname + '&bucket_url=' + url + '&product_aisle=' + str(paisle))
+
+time.sleep(2)
+# TODO: 새 창이 아닌 현재 창 띄우기 가능한지 -> 내용만 바꿔서 실행하면 현재 창의 내용이 바뀜
+driver.get('file:///C:/Users/blue8/OneDrive/Desktop/sns/index.html?user_id=' + str(uid) + '&user_name=' + "팡" + '&product_name=' + pname + '&bucket_url=' + url + '&product_aisle=' + str(paisle))
+
+# 창 끄기
+# driver.close()
