429340cb24ecd7ef20d917106a45bfa524ad9815
Jigu <jsmin0415@gmail.com>
2019-05-11 15:56:03 +0900

    store necessary information to the DB

--- src/recommendation/aws_dynamodb/add_item_to_dynamodb.py
+++ src/recommendation/aws_dynamodb/add_item_to_dynamodb.py
@@ -1,8 +1,7 @@
-# simple code to add item to db
-
 from __future__ import print_function # Python 2/3 compatibility
 import boto3
 import pandas as pd
+import MySQLdb
 
 def add_item(table, user_id, product_id):
     table.put_item(
@@ -15,16 +14,54 @@ def add_item(table, user_id, product_id):
 
 
 def main():
+
+    # variable to connect to DB
+    db = MySQLdb.connect(host="..rds.amazonaws.com",# host
+                     user="",         # username
+                     passwd="",  # password
+                     db="")   # RDS database name
+
+    # data which stores recommendation info
+    recommendation = pd.read_csv("user_1.csv")
+
+    # get data from RDS, to show additional info to users
+    product = pd.read_sql('SELECT product_id,product_name,aisle_id  FROM products', con=db)
+    aisle= pd.read_sql('SELECT * FROM aisles', con=db)
+
+    # columns in RDS  data
+    print(product.columns)
+    print(aisle.columns)
+
+    # print a part of recommendation info
+    #print(recommendation[0:30])
+
+    print(type(recommendation.loc[0]['product_id']))
+    print(type(product.loc[0]['product_id']))
+
+
+    # remove rows if it takes no recommended product
+    recommendation = recommendation[recommendation.product_id!="None"]
+
+    # to merge data, change to the same data type
+    recommendation['product_id'].apply(int)
+    recommendation['product_id']=recommendation['product_id'].astype(int)
+
+    # result data to store in DB
+    result = pd.merge(recommendation, product, how='inner', on=['product_id'])
+    result = pd.merge(result, aisle, how='inner', on=['aisle_id'])
+
+    # print a part of result data to check
+    print(result.head())
+
     # connect to dynamodb
     dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
     # select table to add item
     table = dynamodb.Table('Recommendation')
-    # the file which contains items
-    recommendation = pd.read_csv("./xgboost_submission.csv")
-   
-    # add all item iteratively
-    for i in range(recommendation.shape[0]):
-        add_item(table, recommendation.loc[i]['user_id'], recommendation.loc[i]['product_id'])
+
+
+    # add items to dynamoDB
+    for i in range(result.shape[0]):
+        add_item(table, result.loc[i]['user_id'], result.loc[i]['product_id'])
 
 if __name__ == "__main__":
     main()
