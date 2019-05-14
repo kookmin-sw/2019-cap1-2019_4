a9686bbb165b1075da5b82460b73adca29bd98ce
Jigu <jsmin0415@gmail.com>
2019-05-14 13:44:09 +0900

    store more information
    product_name, aisle_name, bucket_url

--- src/recommendation/aws_dynamodb/add_item_to_dynamodb.py
+++ src/recommendation/aws_dynamodb/add_item_to_dynamodb.py
@@ -4,12 +4,16 @@ import pandas as pd
 import MySQLdb
 import sys
 
-def add_item(table, user_id, product_id):
+
+def add_item(table, user_id, product_id, product_name, aisle_name):
     table.put_item(
         Item={
         'user_id': user_id,
         'update_ver' : 1,
-        'product_id': product_id
+        'product_id': product_id,
+        'product_name' : product_name,
+        'aisle_name' : aisle_name,
+        'bucket_url': "https://s3-us-west-2.amazonaws.com/kmu-ads-bucket/"+ str(product_id) + ".png"
         }
     )
 
@@ -17,9 +21,9 @@ def add_item(table, user_id, product_id):
 def main():
 
     # variable to connect to DB
-    db = MySQLdb.connect(host="flexadsdb.cklcaxnrw3b0.us-east-2.rds.amazonaws.com",# host
-                     user="wldud8463",         # username
-                     passwd="dbswldud8!",  # password
+    db = MySQLdb.connect(host="rds.amazonaws.com",# host
+                     user="",         # username
+                     passwd="",  # password
                      db="Flex_ads")   # RDS database name
 
     # data which stores recommendation info
@@ -56,15 +60,15 @@ def main():
 
     # connect to dynamodb
     dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
+
+
     # select table to add item
     table = dynamodb.Table('Recommendation')
-
 
     # add items to dynamoDB
     for i in range(result.shape[0]):
         print(i)
-        add_item(table, result.loc[i]['user_id'], result.loc[i]['product_id'])
+        add_item(table, result.loc[i]['user_id'], result.loc[i]['product_id'], result.loc[i]['product_name'], result.loc[i]['aisle'])
 
 if __name__ == "__main__":
     main()
-
