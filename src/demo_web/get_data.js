c8c99d527d5b0e12df658bdbff3f8f151e009916
HwangSoojin <blue8959@naver.com>
2019-05-15 17:18:04 +0900

    function 을 이용해서 정보(고객, 제품 등)만을 처리하기

--- src/demo_web/get_data.js
+++ src/demo_web/get_data.js
@@ -25,12 +25,31 @@ var qs = parse_query_string(query);
 
 var user_id = qs.user
 var user_name = qs.user_name
-var product_id = qs.product_id
 var product_name = qs.product_name
 var bucket_url = qs.bucket_url
-var product_aisle = 43
+var product_aisle = qs.product_aisle
+var current_time = qs.current_time
 
-// HTML 내용 구성
-document.write( '<p> <span style="color: DarkViolet; font-size:20px;"><b>' + user_name + '</b></span> 고객님 ' );
-document.write( '<span style="color: MediumVioletRed; font-size:20px;"><b>' + product_name + '</b></span>은 어떠세요? <span style="color: SaddleBrown;  font-size:20px;"><b>' + product_aisle + '</b></span>번 코너에 준비되어 있습니다.<br></p>' );
-document.write('<img src=' + bucket_url + ' alt="ad">')
+function write_user_name() {
+  var temp = user_name
+  document.write(temp.fontcolor("DodgerBlue"))
+}
+
+function write_product_name() {
+  var temp = product_name
+  document.write(temp.fontcolor("Red"))
+}
+
+function write_product_aisle() {
+  var temp = product_aisle
+  document.write(temp.fontcolor("Orange"))
+}
+
+function write_current_time() {
+  var temp = current_time
+  document.write(temp.fontcolor("Gray"))
+}
+
+function write_image() {
+  document.write('<img src=' + bucket_url + ' width="100%" alt="ad">')
+}
