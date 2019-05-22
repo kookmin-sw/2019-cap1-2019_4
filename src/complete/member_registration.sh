e764c98c5d74c5dcc28e4792291e2d4bd8a1ce07
Sungjae Lee <odobenuseKR@gmail.com>
2019-05-22 16:13:07 +0900

    add python parameter and sleep, clear

--- src/complete/member_registration.sh
+++ src/complete/member_registration.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-echo -----------------------------------
+echo
+echo ----------------------------------
 echo Flex Ads Member Registration System
 echo -----------------------------------
 echo
@@ -13,11 +13,18 @@ echo
 
 # make user dataset directory
 mkdir $user_name
-echo \' $user_name \' directory created!
+echo \'$user_name\' directory created!
 ls
 
 #copy detectnet to user dataset directory
 cp ./detectnet-camera $user_name
+
+echo
+echo ------------------------------------
+echo Start make face dataset with facenet
+echo ------------------------------------
+echo
+sleep 5
 
 # change directory and run face detection
 # with timeout 30s, detect and save face image in 30s
@@ -26,13 +33,14 @@ timeout 30s ./detectnet-camera facenet
 
 # remove detectnet-camera program from user dataset directory
 rm ./detectnet-camera
+clear
 
 # add user_name to DynamoDB with user_id
 cd ../
 python3 username_dynamodb.py $user_name $user_id
 
 # face indexing to rekognition with user face dataset
-python3 rekognition_indexing.py $user_name
+python3 rekognition_indexing.py $user_id
 
 echo
 echo ++++++++++++++++++++++++++++++++++++
