dceb6378281e2b867718c7b7ce04f6aa9ebb4116
wldud8463 <48198249+wldud8463@users.noreply.github.com>
2019-05-23 15:58:50 +0900

    Update dowRank_and_prodCount.py

--- src/recommendation/make_feature/dowRank_and_prodCount.py
+++ src/recommendation/make_feature/dowRank_and_prodCount.py
@@ -12,8 +12,18 @@ import re
 """
 def dow_prod():
     
-    orders_df = pd.read_csv("instacart-market-basket-analysis/orders.csv", usecols=["order_id","order_dow"])
-    prior_df = pd.read_csv("instacart-market-basket-analysis/order_products__prior.csv", usecols = ["order_id", "product_id"])
+    # data from RDS -----------------------------------------------
+    db = pymysql.connect(host="", port= , user="",passwd ="", db="")
+    
+    SQL = "SELECT order_id, order_dow FROM orders"
+    orders_df = pd.read_sql(SQL, db)
+    del SQL
+    
+    SQL = "SELECT order_id, product_id FROM order_products__prior"
+    prior_df = pd.read_sql(SQL,db)
+    del SQL
+    # -------------------------------------------------------------
+    
     
     #merge
     prior_and_orders = pd.merge(prior_df, orders_df, how='inner', on=['order_id'])
