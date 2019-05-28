7c8bb56f0ee7c733cb702be780b827db56e83648
jiwon <aaa0265@naver.com>
2019-05-28 11:29:26 +0900

    Update feature.py

--- src/recommendation/run/feature.py
+++ src/recommendation/run/feature.py
@@ -25,6 +25,39 @@ from xgboost import plot_tree
 ###################### DB connect
 db = pymysql.connect(host="", port=3306, user="", passwd="",db="")
 
+## 구매하는 상품에 대해서 회원이 재구매를 했었는지, 얼마나 샀었는지 알 수 있는 함수.
+def latest_order():
+    
+    ## 주문 정보가 있는 테이블에서 불러 올 것
+    SQL = "SELECT order_id, user_id, order_number FROM orders"
+    orders_df = pd.read_sql(SQL, db)
+  
+    ## 회원의 구매 내역에서 불러 올 것
+    SQL = "SELECT * FROM order_products__prior"
+    prior_df = pd.read_sql(SQL, db)
+   
+    # merge
+    prior_df = pd.merge(prior_df, orders_df, how="inner", on="order_id")
+
+    # 최근 구매한 기록 - 제일 큰 order_id 찾고, 최근 구매한 상품 목록 찾기
+    prior_grouped_df = prior_df.groupby("user_id")["order_number"].aggregate("max").reset_index()
+    prior_df_latest = pd.merge(prior_df, prior_grouped_df, how="inner", on=["user_id", "order_number"])
+
+    # 칼럼 이름 변경 reordered >> reordered_latest
+    prior_df_latest = prior_df_latest[["user_id", "product_id", "reordered"]]
+    prior_df_latest.columns = ["user_id", "product_id", "reordered_latest"]
+
+    # 유저가 구매한 상품들이 몇번 구매됐고 재구매는 몇번 되었는지
+    prior_df = prior_df.groupby(["user_id","product_id"])["reordered"].aggregate(["count", "sum"]).reset_index()
+
+    # 칼럼명 바꾸기!
+    prior_df.columns = ["user_id", "product_id", "reordered_count", "reordered_sum"]
+
+    # merge prior_df & latest df
+    latest_order = pd.merge(prior_df, prior_df_latest, how="left", on=["user_id","product_id"])
+
+    # 유저 아이디로 나중에 머지 할 것임!!
+    return latest_order
 
 
 ### 상품, 유저 아이디로 뼈대와 결합
